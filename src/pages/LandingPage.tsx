1deb13a8fbdd3e13690ac29d1c8874ae538639b9
Christian <ps5christian@hotmail.com>
2024-12-08 22:45:27 +0100

    #5 made the icon component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,17 +1,22 @@
 import { Button } from "../components/Button/Button";
+import { Icon } from "../components/Icon/Icon";
+import { Wrapper } from "../components/Wrapper/Wrapper";
+import { IoMoonSharp } from "react-icons/io5";
 
 export const LandingPage = () => {
   const handleClick = () => {
     console.log("lol");
   };
   return (
-    <div>
-      LandingPage
+    <Wrapper styling="flex-column">
       <Button
         action={handleClick}
         type="m-btn border-none sharp-btn submit-btn"
         text="Click me!"
       />
-    </div>
+      <Icon action={handleClick} style="m-icon">
+        <IoMoonSharp />
+      </Icon>
+    </Wrapper>
   );
 };
